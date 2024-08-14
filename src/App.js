3c41a593ead99e9bd985d9ba9fca530597b68d24
khderalromi <www.khderalromi999@gmail.com>
2024-08-14 14:17:25 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/CheckOut/CheckOut';
-import CheckoutSummary from './components/Order/checkoutSummary/checkoutSummary';
-import Orders from './containers/Orders/Orders';
-import { Route,Routes } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./component/Pages/HomePage/HomePage";
+import About from "./component/Pages/About/About";
+import ContactUs from "./component/Pages/ContactUs/ContactUs";
+import SignUp from "./component/Pages/SignUp/SignUp";
+import LogIn from "./component/Pages/LogIn/LogIn";
+import UserCart from "./component/Pages/UserCart/User-Cart";
+import { useEffect } from "react";
+import { fetchProducts } from "././store/Products-actions";
+import { useDispatch } from "react-redux";
+
+function App() {
+  
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(fetchProducts())
+  },[dispatch]) 
 
 
+  return (
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/Home' element={<HomePage/>} />
+          <Route path='/About' element={<About/>} />
+          <Route path='/Contact' element={<ContactUs/>} />
+          <Route path='/Sign Up' element={<SignUp/>} />
+          <Route path='/login' element={<LogIn/>} />
+          <Route path='/cart' element={<UserCart/>} />
+        </Routes>
 
-class App extends Component {
-  render() {
-    return (
-
-        <div className="App">
-          <Layout>
-            <Routes>
-                <Route path='/' element={<BurgerBuilder/>} />
-                <Route  path='/checkout/*' element={<Checkout/>} />
-                <Route  path='/orders' element={<Orders/> } />
-   
-            </Routes>
-        
-          
-          </Layout>
-        </div>
- 
-      
-    );
-  }
+      </div>
+  );
 }
 
 export default App;
--- src/axios-order.js
+++ src/axios-order.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance= axios.create({
-    baseURL:'https://burger-77135-default-rtdb.firebaseio.com/'
+    baseURL:'https://fakestoreapi.com/products/'
 });
 
 export default instance;
